bbdacde688743a68ee7989d4fc9615423e80d09c
Talha Ahmed <24100033@lums.edu.pk>
2023-08-31 00:08:13 +0500

    add scripts

---
+++ python_scripts/logs_and_results.py
@@ -0,0 +1,89 @@
+import datetime
+import os
+import time
+
+ROOT = '/content/drive/MyDrive/DUPA - RCPA/Technology transfer deep unfolding/SPROJ-ConvMC-Net/Sensor Data'
+TRY = '1st try'
+SESSION = 1
+
+# Small Helper Function to modularize training loop
+
+# Short functions to get sampling rate and noise variables as strings. make_dir is a function which takes the appropriate noise and sampling rate directory and makes there a directory for either logs or
+# saved models or plots (for both convmc and admm). get_modularized_record() takes the project name, hyperparameters, q and sigma and returns its file path either that be a log file or a title for a plot
+def get_q_str(q):
+  q_exp = f'{q * 100}%'
+  return q_exp
+
+def get_noise_str(sigma):
+  noise_exp = f'{float(sigma)}'
+  return noise_exp
+
+def make_dir(q, sigma, new_entry, hyper_param_net):
+  new_dir = ROOT + '/Q is ' + get_q_str(q) + '/Noise Variance ' + get_noise_str(sigma) + '/' + new_entry
+  os.makedirs(new_dir, exist_ok = True)
+
+  convmc_dir = new_dir + '/ConvMC-Net'
+  os.makedirs(convmc_dir, exist_ok = True)
+
+  admm_dir = new_dir + '/ADMM-Net'
+  os.makedirs(admm_dir, exist_ok = True)
+
+  if hyper_param_net['Model'] == 'ADMM-Net':
+    return admm_dir
+  else:
+    return convmc_dir
+
+# Making a small function which makes directories for different tries for different models
+def make_session(q, sigma, new_entry, hyper_param_net, session_no):
+  # Get dir of activity/record to make sessions of
+  dir = make_dir(q, sigma, new_entry, hyper_param_net)
+
+  # Now make a session corresponding to whichever try is going on currently
+  session_dir = dir + '/Session 1'
+  os.makedirs(session_dir, exist_ok = True)
+
+  return session_dir
+
+# Making a small function to get date and time according to our time zone. Will be used to create the logs
+def get_current_time():
+  ts = time.time()
+  st = datetime.datetime.fromtimestamp(ts)
+
+  # Add 5 hours to the datetime according to our time zone
+  new_time = st + datetime.timedelta(hours = 5)
+
+  formatted_new_time = new_time.strftime('%Y-%m-%d %H:%M:%S')
+
+  return formatted_new_time
+
+def make_predictions_dir(project_name, q, sigma, new_entry, params_net, hyper_param_net, SESSION):
+  # Get Directory with ADMM-Net/ConvMC-Net --> Session directories built
+  dir = make_session(q, sigma, new_entry, hyper_param_net, SESSION)
+
+  # Make a children directory after Session --> ProjectName + parameters --> Train/Test which will contain the final predictions on our train and test data used to evaluate the model after training phase
+  param_project_dir = (f'{dir}/{project_name} Layers_{params_net["layers"]}_Alpha_{hyper_param_net["Alpha"]:.2f}_TrainInstances_{hyper_param_net["TrainInstances"]}_Epochs_{hyper_param_net["Epochs"]}_lr_{hyper_param_net["Lr"]}')
+  os.makedirs(param_project_dir, exist_ok = True)
+
+  # Train/Test Dir dir
+  train_dir = param_project_dir + '/train'
+  test_dir = param_project_dir + '/test'
+
+  os.makedirs(train_dir, exist_ok = True)
+  os.makedirs(test_dir, exist_ok = True)
+
+  # Return Train and Test Directories to store data in
+  return train_dir, test_dir
+
+def get_modularized_record(project_name, q, sigma, new_entry, hyper_param_net, params_net, current_epoch = None):
+
+  # Get directory
+  dir = make_session(q, sigma, new_entry, hyper_param_net, SESSION)
+  if current_epoch == None:
+    log_path = (f'{dir}/{project_name} Layers_{params_net["layers"]}_Alpha_{hyper_param_net["Alpha"]:.2f}_TrainInstances_{hyper_param_net["TrainInstances"]}_Epochs_{hyper_param_net["Epochs"]}_lr_{hyper_param_net["Lr"]}.txt')
+    return log_path
+  elif new_entry == 'Plots':
+    model_path = (f'{dir}/{project_name} Layers_{params_net["layers"]}_Alpha_{hyper_param_net["Alpha"]:.2f}_TrainInstances_{hyper_param_net["TrainInstances"]}_Epochs_[{current_epoch}_out_of_{hyper_param_net["Epochs"]}]_lr_{hyper_param_net["Lr"]}.png')
+    return model_path
+  else:
+    model_path = (f'{dir}/{project_name} Layers_{params_net["layers"]}_Alpha_{hyper_param_net["Alpha"]:.2f}_TrainInstances_{hyper_param_net["TrainInstances"]}_Epochs_[{current_epoch}_out_of_{hyper_param_net["Epochs"]}]_lr_{hyper_param_net["Lr"]}.pth')
+    return model_path
---
+++ python_scripts/training.py
@@ -0,0 +1,126 @@
+import torch
+from convmc import to_var, UnfoldedNet3dC
+
+# Defining a function which sets up the default state of the parameters before training
+def get_default_param(gpu = True):
+  params_net = {}
+  params_net['layers'] = 5
+
+  params_net['initial_neta'] = 1.81    # fixed
+  params_net['initial_lamda1'] = 0.051 # fixed
+  params_net['initial_lamda2'] = 0.049 # fixed
+  params_net['initial_v'] = 0
+
+  params_net['initial_S'] = 0.05001 #fixed
+
+  params_net['initial_P'] = 0.2401 #fixed
+
+  params_net['coef_gamma'] = 0.4001
+
+  params_net['CalInGPU'] = gpu #whether to calculate in GPU
+  params_net['size1'] = 49
+  params_net['size2'] = 60
+
+  return params_net
+
+def get_hyperparameter_grid(Model, TrainInstances, ValInstances, BatchSize, ValBatchSize, Alpha, num_epochs, learning_rate):
+  hyper_param = {}
+
+  hyper_param['Model'] = Model
+  hyper_param['TrainInstances'] = TrainInstances
+  hyper_param['ValInstances'] = ValInstances
+  hyper_param['BatchSize'] = BatchSize
+  hyper_param['ValBatchSize'] = ValBatchSize
+  hyper_param['Alpha'] = Alpha
+  hyper_param['Epochs'] = num_epochs
+  hyper_param['Lr'] = learning_rate
+
+  return hyper_param
+
+# Defining a fucntion for loading/instantiating the model based on some boolean values and updates log
+def get_model(params_net, log, path_whole = None, path_dict = None, loadmodel = False, load_frm = None):
+    # Construct Model
+    print('Configuring Network...\n')
+    log.write('Configuring network...\n')
+
+    if not loadmodel:
+      print('Instantiating Model...\n')
+      log.write('Instantiating Model...\n')
+      net = UnfoldedNet3dC(params_net)
+      print('Model Instantiated...\n')
+      log.write('Model Instantiated...\n')
+
+    else:
+        print('Loading Model...\n')
+        log.write('Loading Model...\n')
+        if load_frm == 'state_dict':
+          net = UnfoldedNet3dC(params_net)
+          state_dict = torch.load(path_dict, map_location = 'cpu')
+          net.load_state_dict(state_dict)
+          print('Model loaded from state dict...\n')
+          log.write('Model loaded from state dict...\n')
+        elif load_frm == 'whole_model':
+          net = torch.load(path_whole)
+          print('Whole model loaded...\n')
+          log.write('Whole model loaded...\n')
+        net.eval()
+
+    net = net.cuda()
+
+    return net
+
+# Training functions and Plotting Functions
+
+def train_step(model, dataloader, loss_fn, optimizer, log, CalInGPU, Alpha, TrainInstances, batch):
+  # Put model in train mode
+  model.train()
+
+  # Initalize loss for lowrank matrices which will be calculated per batch for each epoch
+  loss_mean = 0
+  loss_lowrank_mean = 0
+
+  for _, (D, L) in enumerate(dataloader):
+    # set the gradients to zero at the beginning of each epoch
+    optimizer.zero_grad()
+    with torch.autograd.set_detect_anomaly(False):
+      for ii in range(batch):
+          inputs1 = to_var(D[ii], CalInGPU)
+          targets_L = to_var(L[ii], CalInGPU)
+          # Forward + backward + loss
+          lst_1 = model([inputs1])
+          outputs_L = lst_1[0][1]
+          # Current loss
+          loss = (Alpha * loss_fn(outputs_L, targets_L))/torch.square(torch.norm(targets_L, p = 'fro'))
+          loss_lowrank = (loss_fn(outputs_L,targets_L))/torch.square(torch.norm(targets_L, p = 'fro'))
+          loss_mean += loss.item()
+          loss_lowrank_mean += loss_lowrank.item()
+          loss.backward()
+    optimizer.step()
+  loss_mean = loss_mean/TrainInstances
+  loss_lowrank_mean = loss_lowrank_mean/TrainInstances
+
+  return loss_mean, loss_lowrank_mean
+
+def test_step(model, dataloader, loss_fn, optimizer, log, CalInGPU, Alpha, ValInstances, batch):
+
+  model.eval()
+  loss_val_mean, loss_val_lowrank_mean = 0, 0
+
+  # Validation
+  with torch.no_grad():
+    for _, (Dv, Lv) in enumerate(dataloader):
+      for jj in range(batch):
+        inputsv1 = to_var(Dv[jj], CalInGPU)   # "jj"th picture
+        targets_Lv = to_var(Lv[jj], CalInGPU)
+        lst_2 = model([inputsv1])  # Forward
+        outputs_Lv = lst_2[0][1]
+        # Current loss
+        loss_val = Alpha * loss_fn(outputs_Lv, targets_Lv)/torch.square(torch.norm(targets_Lv, p = 'fro'))
+        loss_val_lowrank = loss_fn(outputs_Lv, targets_Lv)/torch.square(torch.norm(targets_Lv, p = 'fro'))
+        loss_val_mean += loss_val.item()
+        loss_val_lowrank_mean += loss_val_lowrank.item()
+
+  loss_val_mean = loss_val_mean/ValInstances
+  loss_val_lowrank_mean = loss_val_lowrank_mean/ValInstances
+
+  return loss_val_mean, loss_val_lowrank_mean
