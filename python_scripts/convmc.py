16e1b481c379c00672182040723b9fff10faf9e3
Talha Ahmed <122668359+TalhaAhmed2000@users.noreply.github.com>
2023-09-08 21:57:23 +0500

    Update convmc.py

--- python_scripts/convmc.py
+++ python_scripts/convmc.py
@@ -8,7 +8,7 @@ def to_var(X, CalInGPU):
     if CalInGPU and torch.cuda.is_available():
         X = X.cuda()
         X.requires_grad = True
-    return X
+    return Variable(X)
 
 
 class ISTACell_admm(nn.Module):
