3837ae42d2c6017439838329fe2f785fd554ac07
Talha Ahmed <122668359+TalhaAhmed2000@users.noreply.github.com>
2023-09-08 22:12:49 +0500

    Update convmc.py

--- python_scripts/convmc.py
+++ python_scripts/convmc.py
@@ -7,8 +7,7 @@ from torch.autograd import Variable
 def to_var(X, CalInGPU):
     if CalInGPU and torch.cuda.is_available():
         X = X.cuda()
-        X.requires_grad = True
-    return X
+    return Variable(X)
 
 
 class ISTACell_admm(nn.Module):
