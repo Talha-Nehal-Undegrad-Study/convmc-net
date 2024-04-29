06ae3141e89dcfc13aa73d9d207712af83f5f28b
Talha Ahmed <24100033@lums.edu.pk>
2024-04-29 23:12:28 +0500

    update

---
+++ image_py_scripts/dataset_processing.py
@@ -0,0 +1,76 @@
+import numpy as np
+import torch
+import torch.utils.data as data
+from image_py_scripts import generate_synthetic_data
+from image_py_scripts import format_data
+
+# ROOT = 'C:/Users/Talha/OneDrive - Higher Education Commission/Documents/GitHub/ConvHuberMC/HuberMC_Data'
+# ROOT = 'C:/Users/HP/Git/ConvHuberMC-Net/HuberMC_Data'
+
+# def preprocess(L, D, size1, size2, size3):
+
+#     A = max(np.max(np.abs(L)), np.max(np.abs(D)))
+#     if A == 0:
+#         A = 1
+#     L = L/A
+#     D = D/A
+
+#     return L, D
+
+class ImageDataset(data.Dataset):
+    def __init__(self, NumInstances, shape, split, ROOT, transform = None):
+        self.shape = shape
+
+        # dummy image loader
+        images_L = torch.zeros(tuple([NumInstances]) + self.shape) # --> shape: (400, 49, 60)
+        images_D = torch.zeros(tuple([NumInstances]) + self.shape) # --> shape: (400, 49, 60)
+
+        # TRAIN
+        if split == 0:
+            for n in range(NumInstances):
+                L = np.load(ROOT + '/lowrank/train/L_mat_MC_train' + str(n + 1) + '.npy')
+                D = np.load(ROOT + '/groundtruth/train/ground_mat_MC_train' + str(n + 1) + '.npy')
+                # L, D = preprocess(L, D, None, None, None)
+
+                images_L[n] = torch.from_numpy(L)
+                images_D[n] = torch.from_numpy(D)
+
+         # TEST
+        if split == 1:
+            for n in range(NumInstances):
+                L = np.load(ROOT + '/lowrank/test/L_mat_MC_test' + str(n + 1) + '.npy')
+                D = np.load(ROOT + '/groundtruth/test/ground_mat_MC_test' + str(n + 1) + '.npy')
+                # L, D = preprocess(L, D, None, None, None)
+
+                images_L[n] = torch.from_numpy(L)
+                images_D[n] = torch.from_numpy(D)
+
+
+        self.transform = transform
+        self.images_L = images_L
+        self.images_D = images_D
+
+    def __getitem__(self, index):
+        L = self.images_L[index]
+        D = self.images_D[index]
+        return L, D
+
+    def __len__(self):
+        return len(self.images_L)
+
+
+def get_dataloaders(params_net, hyper_param_net, sampling_rate, db, ROOT):
+    M_train, M_Omega_train, M_test, M_Omega_test = generate_synthetic_data.generate(
+        params_net['size1'], params_net['size2'], params_net['rank'], 
+        hyper_param_net['TrainInstances'], hyper_param_net['ValInstances'], sampling_rate, db)
+
+    # Format and Save Data
+    format_data.format(M_train, M_Omega_train, M_test, M_Omega_test, ROOT)
+
+    # Create DataLoaders
+    train_dataset = ImageDataset(hyper_param_net['TrainInstances'], (params_net['size1'], params_net['size2']), 0, ROOT)
+    train_loader = data.DataLoader(train_dataset, batch_size = 5, shuffle = True)
+    test_dataset = ImageDataset(hyper_param_net['ValInstances'], (params_net['size1'], params_net['size2']), 1, ROOT)
+    test_loader = data.DataLoader(test_dataset, batch_size = 2)
+
+    return train_loader, test_loader
---
+++ python_scripts/run_image_inpainting.py
@@ -0,0 +1,49 @@
+import numpy as np
+from skimage import io, color, img_as_float
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+from scipy.stats import binom, norm
+from scipy.optimize import minimize
+
+
+# Function to implement image inpainting
+def image_inpainting(M, M_Omega, rak, maxiter, model):
+    # Placeholder function for image inpainting
+    # Replace with your image inpainting implementation
+    PSNR = psnr(M, M_Omega)
+    SSIM = ssim(M, M_Omega)
+    return PSNR, SSIM
+
+# Parameters
+r, c, rak = 400, 500, 10
+dB = 5
+per = 0.5
+models = ['L0-BCD', 'Lp-reg', 'Lp-ADMM', 'ORMC', 'M-Estimation']
+PSNRs = np.zeros((len(models), 8))
+SSIMs = np.zeros((len(models), 8))
+
+# Loop through models and images
+for m in range(len(models)):
+    for i in range(1, 9):
+        # Read Image and convert into black and white and reshape into 150 x 300
+        # Replace the path with your own
+        image_path = f'C:/Users/Talha/OneDrive - Higher Education Commission/Documents/GitHub/M-estimation-RMC/M-Estimation/Image_Inpainting_Dataset/{i}.jpg'
+        M = io.imread(image_path)
+        M = color.rgb2gray(img_as_float(M))
+        M = np.resize(M, (r, c))
+
+        # Add GMM noise
+        array_Omega = binom.rvs(1, per, size=(r, c))
+        M_Omega = M * array_Omega
+        omega = np.where(array_Omega == 1)
+        noise = gaussian_noise(M_Omega[omega], 1, dB)
+        Noise = np.zeros_like(M_Omega)
+        Noise[omega] = noise
+        M_Omega = M_Omega + Noise
+        maxiter = 50
+
+        PSNR, SSIM = image_inpainting(M, M_Omega, rak, maxiter, models[m])
+        print(f'Model: {models[m]}, PSNR of Image {i} = {PSNR}, SSIM of Image {i} = {SSIM}')
+
+        PSNRs[m, i-1] = PSNR
+        SSIMs[m, i-1] = SSIM
